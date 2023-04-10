978712fd532b5bc305500cd309bbcebffb18d436
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2023-04-10 12:13:06 +1000

    Added button password checking functionality and side logo with gif

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const password = document.querySelector('#password');
 const confirmedPassword = document.querySelector('#confirmed-password');
+const mainButton = document.querySelector('#main-button');
 
 let checkMatch = (fieldOne, fieldTwo) => {
     if (fieldOne.value === fieldTwo.value) {
-        alert("There is a match");
-    };
+        return true;
+    }
 };
 
-confirmedPassword.addEventListener("input", function() {
-    checkMatch(password, confirmedPassword);
+let warnMatch = (fieldOne, fieldTwo) => {
+    if (!checkMatch(fieldOne, fieldTwo)) {
+        fieldTwo.style.border = "solid 2px red";
+       } else {
+        fieldTwo.style.border = "solid 3px green";
+       }
+}
+
+confirmedPassword.addEventListener("input", () => {
+    warnMatch(password, confirmedPassword);
+});
+
+mainButton.addEventListener("click", (event) => {
+    if (!checkMatch(password, confirmedPassword)) {
+        warnMatch(password, confirmedPassword);
+        event.preventDefault();
+        alert("The passwords do not match");
+    };
 });
